249db6627ecec8432bc85119408e9dab8ad769c0
gemyzero <mohamed0110gemy@gmail.com>
2025-01-03 02:07:35 +0200

    therd commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.scss'
+import { FaFacebookF, FaInstagram  , FaTwitter  ,FaLinkedinIn} from "react-icons/fa";
 export default function Header() {
   return (
     <div>
@@ -17,12 +18,12 @@ export default function Header() {
       <div className="icon-content">
 <ul>
   <li>
-  <a href="https://www.facebook.com/profile.php?id=100043474942924"><i className="fa-brands fa-facebook"></i> </a>
+  <a href="https://www.facebook.com/profile.php?id=100043474942924"><FaFacebookF /></a>
 </li>
-<li><a href="https://x.com/mohamedGmalZero">  <i className="fa-brands fa-twitter"></i></a> 
+<li className=''><a href="https://x.com/mohamedGmalZero"> <FaTwitter /></a>
  </li>
-<li>   <a href="https://www.instagram.com/mohamedjim2022/"><i className="fa-brands fa-instagram"></i></a></li>
-<li><a href="https://www.linkedin.com/in/mogamed-gmal-93a42b33a/"><i className="fa-brands fa-linkedin"></i></a>
+<li>   <a href="https://www.instagram.com/mohamedjim2022/"><FaInstagram /></a></li>
+<li><a href="https://www.linkedin.com/in/mogamed-gmal-93a42b33a/"><FaLinkedinIn /></a>
 </li>
 </ul>
 
